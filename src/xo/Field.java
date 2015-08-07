dca186c407a7c70dd67d5751eb15e46eff45f0a6
Daniil <dgaponov@lenta.ru>
2015-08-07 19:34:46 +0600

    Field - created

---
+++ src/xo/Field.java
@@ -0,0 +1,26 @@
+package xo;
+
+import java.awt.*;
+
+/**
+ * Created by Даниил on 07.08.2015.
+ */
+public class Field {
+
+    private static final int FIELD_SIZE = 3;
+
+    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+
+    public int getFieldSize() {
+        return FIELD_SIZE;
+    }
+
+    public Figure getFigure(final Point point) {
+        return field[point.x][point.y];
+    }
+
+    public void setFigure(final Point point, final Figure figure) {
+        field[point.x][point.y] = figure;
+    }
+
+}
