bb5c7fa966fc81285478e5acbd37cdd37fdc17c2
Daniil <dgaponov@lenta.ru>
2015-08-07 19:16:02 +0600

    Player - created

---
+++ src/xo/Player.java
@@ -0,0 +1,25 @@
+package xo;
+
+/**
+ * Created by Даниил on 07.08.2015.
+ */
+public class Player {
+
+    private final String name;
+
+    private final Figure figure;
+
+    public Player(String name,
+                  Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+}
