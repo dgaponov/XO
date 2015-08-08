37e91f4a7ca43ffd3468ee8f12164ebed79645de
Daniil <dgaponov@lenta.ru>
2015-08-08 15:36:51 +0600

    WinnerControllerTest added

---
+++ tst/controllers/WinnerControllerTest.java
@@ -0,0 +1,84 @@
+package controllers;
+
+import org.junit.Test;
+import xo.Field;
+import xo.Figure;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Даниил on 08.08.2015.
+ */
+public class WinnerControllerTest {
+
+    @Test
+    public void testGetWinnerOnHorizontal() throws Exception {
+        for(int i = 0; i < 3; ++i) {
+            final Field field = new Field(3);
+            final Figure inputFigure = Figure.O;
+
+            field.setFigure(new Point(i, 0), inputFigure);
+            field.setFigure(new Point(i, 1), inputFigure);
+            field.setFigure(new Point(i, 2), inputFigure);
+
+            WinnerController winnerController = new WinnerController();
+            final Figure actualFigure = winnerController.getWinner(field);
+
+            assertEquals(inputFigure, actualFigure);
+        }
+    }
+
+    @Test
+    public void testGetWinnerOnVertical() throws Exception {
+        for(int i = 0; i < 3; ++i) {
+            final Field field = new Field(3);
+            final Figure inputFigure = Figure.O;
+
+            field.setFigure(new Point(0, i), inputFigure);
+            field.setFigure(new Point(1, i), inputFigure);
+            field.setFigure(new Point(2, i), inputFigure);
+
+            WinnerController winnerController = new WinnerController();
+            final Figure actualFigure = winnerController.getWinner(field);
+
+            assertEquals(inputFigure, actualFigure);
+        }
+    }
+
+    @Test
+    public void testGetWinnerOnDiagonalOne() throws Exception {
+        for(int i = 0; i < 3; ++i) {
+            final Field field = new Field(3);
+            final Figure inputFigure = Figure.O;
+
+            field.setFigure(new Point(0, 0), inputFigure);
+            field.setFigure(new Point(1, 1), inputFigure);
+            field.setFigure(new Point(2, 2), inputFigure);
+
+            WinnerController winnerController = new WinnerController();
+            final Figure actualFigure = winnerController.getWinner(field);
+
+            assertEquals(inputFigure, actualFigure);
+        }
+    }
+
+    @Test
+    public void testGetWinnerOnDiagonalTwo() throws Exception {
+        for(int i = 0; i < 3; ++i) {
+            final Field field = new Field(3);
+            final Figure inputFigure = Figure.O;
+
+            field.setFigure(new Point(2, 0), inputFigure);
+            field.setFigure(new Point(1, 1), inputFigure);
+            field.setFigure(new Point(0, 2), inputFigure);
+
+            WinnerController winnerController = new WinnerController();
+            final Figure actualFigure = winnerController.getWinner(field);
+
+            assertEquals(inputFigure, actualFigure);
+        }
+    }
+
+}
