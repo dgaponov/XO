f643ee550c176a921ec4e4ed70672cb87e6f8659
Daniil <dgaponov@lenta.ru>
2015-08-08 17:35:09 +0600

    CurrentMoveControllerTest implement

--- tst/controllers/CurrentMoveControllerTest.java
+++ tst/controllers/CurrentMoveControllerTest.java
@@ -1,13 +1,53 @@
 package controllers;
 
 import org.junit.Test;
+import xo.Field;
+import xo.Figure;
+
+import java.awt.*;
 
 import static org.junit.Assert.*;
 
 public class CurrentMoveControllerTest {
 
     @Test
-    public void testCurrentMove() throws Exception {
+    public void testCurrentMoveWhenNextIsO() throws Exception {
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+        for(int i = 0; i < 3; ++i) {
+            final Field field = new Field(3);
 
+            field.setFigure(new Point(i, 0), Figure.X);
+            field.setFigure(new Point(i, 1), Figure.O);
+            field.setFigure(new Point(i, 2), Figure.X);
+
+            assertEquals(Figure.O, currentMoveController.currentMove(field));
+        }
+    }
+
+    @Test
+    public void testCurrentMoveWhenNextIsX() throws Exception {
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+        for(int i = 0; i < 3; ++i) {
+            final Field field = new Field(3);
+
+            field.setFigure(new Point(i, 0), Figure.X);
+            field.setFigure(new Point(i, 1), Figure.O);
+
+            assertEquals(Figure.X, currentMoveController.currentMove(field));
+        }
+    }
+
+    @Test
+    public void testCurrentMoveWhenNoNextMove() throws Exception {
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+        final Field field = new Field(3);
+
+        for(int i = 0; i < 3; ++i) {
+            field.setFigure(new Point(i, 0), Figure.X);
+            field.setFigure(new Point(i, 1), Figure.O);
+            field.setFigure(new Point(i, 2), Figure.X);
+        }
+
+        assertNull(currentMoveController.currentMove(field));
     }
 }
