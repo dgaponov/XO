57261f824dbe62caa0e7f139ed43a45556d336f1
Daniil <dgaponov@lenta.ru>
2015-08-08 12:49:09 +0600

    Game added

---
+++ src/xo/Game.java
@@ -0,0 +1,35 @@
+package xo;
+
+/**
+ * Created by Даниил on 08.08.2015.
+ */
+public class Game {
+
+    private final Player[] players;
+
+    private final Field field;
+
+    private final String name;
+
+    public Game(Player[] players,
+                Field field,
+                String name) {
+        this.players = players;
+        this.field = field;
+        this.name = name;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
