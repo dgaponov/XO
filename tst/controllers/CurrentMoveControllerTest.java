3099e8443edddeb99fcccaedc45c68962cb4563f
Daniil <dgaponov@lenta.ru>
2015-08-08 17:15:11 +0600

    CurrentMoveControllerTest added

---
+++ tst/controllers/CurrentMoveControllerTest.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CurrentMoveControllerTest {
+
+    @Test
+    public void testCurrentMove() throws Exception {
+
+    }
+}
